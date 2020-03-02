7f9181d872ed40df56f54909ef75beca10f5bb7d
Paul Ridley <paul@pauls-mbp.home>
2020-03-02 14:53:59 +0100

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        String text1 = "how are you doing";
+        System.out.println(text1);
+    }
+}
